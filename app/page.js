9a891ff5cc4a0d4e038aa76bd3f0b79fe07c35c6
Saintdannyyy <danieltesla746@gmail.com>
2025-05-24 04:21:45 +0000

    Refactor Home component to include Header and Hero sections with improved layout

--- app/page.js
+++ app/page.js
@@ -1,6 +1,19 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
-export default function Page() {
-  <Header />;
-  return <h1>Hello Next.js!</h1>;
+export default function Home() {
+  return (
+    <div className="flex z-10 flex-col w-screen h-screen bg-black text-white">
+      {/* Header Section*/}
+      <div className="flex justify-center top-1 w-full bg-[#141301]">
+        <Header className="z-10" />
+      </div>
+      {/* Main Body */}
+      <div className="flex w-full justify-center">
+        {/* Hero Section */}
+        <Hero />
+        {/* About Section */}
+      </div>
+    </div>
+  );
 }
