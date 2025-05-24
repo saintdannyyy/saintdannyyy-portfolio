3bcc407b58ed22cefeb51d79a2f874f07fbd93f2
Saintdannyyy <danieltesla746@gmail.com>
2025-05-24 03:11:03 +0000

    Update Header component styles for improved layout and hover effects

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,37 @@
 export default function Header() {
   return (
-    <header className="flex items-center justify-between p-4 bg-gray-800 text-white">
+    <header className="flex items-center justify-between p-6 text-white">
       <nav>
-        <ul className="flex space-x-4">
+        <ul className="flex space-x-10">
           <li>
-            <a href="#about" className="hover:text-gray-400">
-              AboHomeut
+            <a
+              href="#about"
+              className="hover:text-gray-400 hover:bg-[#EA354699] hover:p-2 rounded-full transition duration-300 ease-in-out"
+            >
+              Home
             </a>
           </li>
           <li>
-            <a href="#about" className="hover:text-gray-400">
+            <a
+              href="#about"
+              className="hover:text-gray-400 hover:bg-[#EA354699] hover:p-2 rounded-full transition duration-300 ease-in-out"
+            >
               About
             </a>
           </li>
           <li>
-            <a href="#projects" className="hover:text-gray-400">
+            <a
+              href="#projects"
+              className="hover:text-gray-400 hover:bg-[#EA354699] hover:p-2 rounded-full transition duration-300 ease-in-out"
+            >
               Projects
             </a>
           </li>
           <li>
-            <a href="#contact" className="hover:text-gray-400">
+            <a
+              href="#contact"
+              className="hover:text-gray-400 hover:bg-[#EA354699] hover:p-2 rounded-full transition duration-300 ease-in-out"
+            >
               Contact
             </a>
           </li>
