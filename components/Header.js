ae0bc62c2b06b86d53c091a1e6f52ac61fa1bb83
Saintdannyyy <danieltesla746@gmail.com>
2025-09-08 05:39:39 +0000

    fix: update profile picture source to use hero image

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
           {/* Profile Picture */}
           <div className="relative w-8 h-8 rounded-full overflow-hidden border-2 border-white/20">
             <Image
-              src="/saintdannyyy.jpg"
+              src="/hero.jpg"
               alt="Saintdannyyy"
               width={50}
               height={50}
