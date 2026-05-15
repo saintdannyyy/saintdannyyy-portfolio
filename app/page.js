bb879f4641913afce54cfa323e766aa6454787e7
Saintdannyyy <danieltesla746@gmail.com>
2026-05-15 13:09:19 +0000

    feat: add Contact component to Home page layout

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import About from "@/components/About";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import TechStack from "@/components/TechStack";
+import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
       <About />
       <Experience />
       <TechStack />
+      <Contact />
       <Footer />
     </div>
   );
